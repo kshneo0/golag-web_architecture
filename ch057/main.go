5b5f15bc6fac82b50b67e17a9299b4ead0174c06
kshneo0 <kshneo@gmail.com>
2022-01-14 06:53:12 +0900

    added context.withTimeout

---
+++ ch057/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func makeRequest(ctx context.Context, route string) (string, error) {
+	deadline, ok := ctx.Deadline()
+	if ok && time.Until(deadline) < 100 * time.Millisecond {
+		return "", fmt.Errorf("Deadline too near")
+	}
+
+	req,err := http.NewRequestWithContext(ctx, http.MethodGet, route, nil)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("Bad status code: %d", resp.StatusCode)
+	}
+
+	bs, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bs), nil
+}
+
+func main(){
+	ctx, cancel := context.WithTimeout(context.Background(),time.Millisecond)
+	defer cancel()
+
+	resp, err := makeRequest(ctx, "https://www.google.com")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(resp)
+}
