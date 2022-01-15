210a241e11d6ac863c303b4ccd416d3e6ef231a8
kshneo0 <kshneo@gmail.com>
2022-01-15 20:10:47 +0900

    ninja 3 ex 5 try number one

---
+++ ch067/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	fmt.Printf("GOROUTINES RUNNING %d\n",runtime.NumGoroutine())
+	ctx := context.Background()
+	ctx, cancelF := context.WithCancel(ctx)
+	defer cancelF()
+
+	for i:=0; i < 100; i++ {
+		go func(n int){
+			fmt.Println("launching goroutine", n)
+			for {
+				select {
+				case <-ctx.Done():
+					runtime.Goexit()				
+				default:
+					fmt.Printf("goroutine %d is doing work\n", n)
+					time.Sleep(50 * time.Millisecond)
+				}
+			}
+		}(i)
+	}
+	time.Sleep(time.Millisecond)
+	fmt.Printf("GOROUTINES RUNNING AFTER ONE MILLISEOND %d\n",runtime.NumGoroutine())
+	cancelF()
+	time.Sleep(100 * time.Millisecond)
+	fmt.Printf("GOROUTINES RUNNING AFTER CANCELFUNC CALLED %d\n",runtime.NumGoroutine())
+
+}
