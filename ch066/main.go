684a9ead409f2d9e0c828977ac3155567b90db27
kshneo0 <kshneo@gmail.com>
2022-01-15 19:39:25 +0900

    ninja 03 ex 04 try 02

---
+++ ch066/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancelF := context.WithTimeout(ctx, 50 * time.Millisecond)
+	defer cancelF()
+
+	time.Sleep(100 * time.Millisecond)
+
+	select {
+	case <-ctx.Done():
+		fmt.Println("work didn't finish")
+	default:
+		fmt.Println("work finished")
+	}
+
+}
