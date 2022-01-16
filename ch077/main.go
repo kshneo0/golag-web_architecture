75f16607fdd8ae06448b92b579ecbd812df060a3
kshneo0 <kshneo@gmail.com>
2022-01-16 14:06:34 +0900

    error comparability

---
+++ ch077/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("rumi.txt")
+
+	if errors.Is(err, os.ErrPermission){
+		err = fmt.Errorf("you do not have permission to open the file: %w", err)
+		log.Println(err)
+	} else if errors.Is(err, os.ErrNotExist){
+		err = fmt.Errorf("the file does not exist: %w", err)
+		log.Println(err)
+	} else if err != nil {
+		err = fmt.Errorf("file couldn't be opened: %w", err)
+		log.Println(err)
+	}
+
+	// if err == os.ErrPermission{
+	// 	err = fmt.Errorf("you do not have permission to open the file: %w", err)
+	// 	log.Println(err)
+	// } else if err == os.ErrNotExist {
+	// 	err = fmt.Errorf("the file does not exist: %w", err)
+	// 	log.Println(err)
+	// } else if err != nil {
+	// 	// panic(err)
+
+	// 	err = fmt.Errorf("file couldn't be opened: %w", err)
+	// 	log.Println(err)
+	// }
+	defer f.Close()
+
+
+}
