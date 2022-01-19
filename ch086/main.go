55616fbeab21de439fb06fbf57d56051497454f1
kshneo0 <kshneo@gmail.com>
2022-01-19 20:18:21 +0900

    ninja 04 ex 01

---
+++ ch086/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type errorString string
+
+func (es errorString) Error() string {
+	return fmt.Sprintf("this is an error string with info about who what where when why how - %ss", string(es))
+}
+
+func main() {
+	n, err := sum(2,4)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(n)
+
+}
+
+func sum(i, j int) (int,error)  {
+	n := i + j
+	if n != i +j {
+		var sErr errorString = "the numbers didn't add up"
+		return 0, sErr
+	}
+	return n, nil
+}
